b46a8e6ca95b71648359a8672e4ab469e69a5c67
anitha225 <ak62181661@gmail.com>
2026-03-20 15:30:19 +0530

    Updated cnn-model.py with the report.

--- cnn-model.py
+++ cnn-model.py
@@ -70,4 +70,14 @@ print("Best Training Accuracy:",
 print("Best Validation Accuracy:",
       max(history_adam.history['val_accuracy']))
 
+#The above Cnn-model showed training accuracy of "86%" and validation accuracy of "79.6%" , hyperparameters tunning has been done step by step ,initially the base model had about "52%" of validation accuracy and was overfitting.
+#In the train model ,more data augmentations techniques were used to improve  generalization and batch-size as been given "32" (32 was selected as it provided optimal balance between training stability and cnn performance),
+#In the cnn_model 'batch-normalization' , 'leakyReLu' , 'maxpooling' , 'conv2D' is added and i am using 'softmax' here for the multi-class classification and 'adam' as the optimizer and early stopping was implementaed to halt training to halt when validation performance stopped improving.
+#Dropout layer played an important role in increasing the accuracy and reducing the overfitting. "0.04" was used earlier but using '0.03" (overfitting was also reduced) gave a rise in the validation accuracy.
+#Changing the Optimizer 'adam' leaning rate also played an important role , also the learning rate was tested from 0.0005 and 0.001 was considered the optimal.
+#Initially 10 epoch was used for testing ,few changes in the data augmentation and adding layers showed an accuracy of "58" and "54" which was decrase in the training accuracy but increase in validation and was not overfitting, later dropout layer and one more layer and leakyReLu maxpooling and early stopping.
+#Overall the accuracy was increased from 52 to 79(the validation accuracy) and the model does not show anymore overfitting.   
+    
+
+
     
