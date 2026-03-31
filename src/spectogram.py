ba7dfd1eb2ea16ad303c15b1b5dab447d9337575
Varsha Tiwari <152866922+varsha2410ab@users.noreply.github.com>
2026-03-31 18:28:14 +0530

    Delete src/spectogram.py

--- src/spectogram.py
+++
@@ -1,6 +0,0 @@
-import librosa
-
-def generate_mel_spectrogram(audio, sr):
-    mel = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=128)
-    log_mel = librosa.power_to_db(mel)
-    return log_mel
